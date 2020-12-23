41eca5463f86aba06a648a6c09798dfa576529d4
felipedsanta <felipe.carro3@Outlook.com>
2020-12-22 22:14:56 -0300

    Update implementado

--- src/application/Program.java
+++ src/application/Program.java
@@ -32,10 +32,16 @@ public class Program {
 		}
 		
 		System.out.println("\n=== TEST 4: seller insert =====");
-		Seller newSeller = new Seller(null, "Felipe", "felipe@gmail.com", new Date(), 1100.0, department);
+		Seller newSeller = new Seller(null, "Telson", "telson@gmail.com", new Date(), 6000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
 	
+		System.out.println("\n=== TEST 5: seller update =====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
+		
 	}
 
 }
